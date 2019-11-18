d0eae05aac1b136efabffc52b888389881dcd84c
LiteralMetaphorical <blesic.andrej@gmail.com>
2019-11-18 23:49:12 +0100

    added sticky header

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
-import MenuButton from '../components/MenuButton';
 
 export default class LinksScreen extends React.Component {
   render() {
@@ -12,7 +11,6 @@ export default class LinksScreen extends React.Component {
          * we just wanted to provide you with some helpful links.
          */}
         <ExpoLinksView />
-        <MenuButton navigation={this.props.navigation} />
       </ScrollView>
     );
   }
