55157d58f90b64741b58170d7387cbd42e0f415a
Edin Dzabija <edin.dzabija@gmail.com>
2019-11-19 16:56:26 +0100

    demo screens

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,29 +1,79 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet, View, Text, Image } from 'react-native';
+import {
+  Ionicons,
+  AntDesign,
+  Zocial,
+  Entypo,
+  Feather
+} from '@expo/vector-icons';
 
-export default class LinksScreen extends React.Component {
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/**
-         * Go ahead and delete ExpoLinksView and replace it with your content;
-         * we just wanted to provide you with some helpful links.
-         */}
-        <ExpoLinksView />
-      </ScrollView>
-    );
-  }
+function Header({ navigation }) {
+  return (
+    <View
+      style={{
+        backgroundColor: '#1e222a',
+        flex: 1,
+        height: 60,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        boxShadow: '10px 10px 10px rgba(0,0,0,1)'
+      }}
+    >
+      <Ionicons
+        name="md-menu"
+        color="#ED7F2C"
+        size={32}
+        style={styles.menuIcon}
+        onPress={() => navigation.toggleDrawer()}
+      />
+      <Image
+        style={{
+          width: 40,
+          height: 40,
+          position: 'absolute',
+          zIndex: 99999,
+          top: 8
+        }}
+        source={require('./CO.png')}
+      />
+      <Feather
+        color="#ED7F2C"
+        style={{ position: 'absolute', zIndex: 99999, top: 13, right: 8 }}
+        size={32}
+        name="user"
+      />
+    </View>
+  );
+}
+export default function LinksScreen({ navigation }) {
+  return (
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      style={styles.container}
+      stickyHeaderIndices={[0]}
+    >
+      <Header style={styles.header} navigation={navigation} />
+    </ScrollView>
+  );
 }
 
 LinksScreen.navigationOptions = {
-  title: 'Linkz'
+  title: 'DEMO 2'
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff'
-  }
+    backgroundColor: '#343942',
+    marginTop: 28
+  },
+  menuIcon: {
+    zIndex: 9,
+    position: 'absolute',
+    top: 13,
+    left: 16
+  },
+  header: {}
 });
